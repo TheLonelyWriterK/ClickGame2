f7bddecbb10a5a60d10c8d82840aa2f3eb356ed1
Kaleb Brown <brown.kaleb2001@gmail.com>
2021-02-10 18:14:44 -0600

    Test

--- Scenes/MainMenu.js
+++ Scenes/MainMenu.js
@@ -10,6 +10,10 @@ class MainMenu extends Phaser.Scene
             this.moneyButton = null;
             // Money Button Text
             this.moneyButtonText = null;
+            // Money
+            this.money = null;
+            // Coins Per Second
+            this.coinsPerSecond = null;
     }
 
     init()
@@ -28,6 +32,11 @@ class MainMenu extends Phaser.Scene
             // Creates the rectangle for the money button
             this.moneyButton = this.add.rectangle(800,450,50,50,0x110022)
 
+            // Sets the "moneyButton" as interactive
+            this.moneyButton.setInteractive()
+
+            // Sets the "moneyButton" as on state of interactive
+            this.moneyButton.on("pointerdown",this.onClickAddMoney,this)
     }
 
     update()
--- Scenes/UpgradeScreen.js
+++ Scenes/UpgradeScreen.js
@@ -10,10 +10,6 @@ class UpgradeScreen extends Phaser.Scene
             this.moneyButton = null;
             // Money Button Text
             this.moneyButtonText = null;
-            // Money
-            this.money = null;
-            // Coins Per Second
-            this.coinsPerSecond = null;
     }
 
     init()
@@ -28,15 +24,7 @@ class UpgradeScreen extends Phaser.Scene
 
     create()
     {
-        // Money Button
-            // Creates the rectangle for the money button
-            this.moneyButton = this.add.rectangle(800,450,50,50,0x110022)
-
-            // Sets the "moneyButton" as interactive
-            this.moneyButton.setInteractive()
-
-            // Sets the "moneyButton" as on state of interactive
-            this.moneyButton.on("pointerdown",this.onClickAddMoney,this)
+        
 
     }
 
